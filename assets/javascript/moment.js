82785abab421b7cda11234d22d0eb8f6b6513a66
Kendall Arceo <arceokendall@gmail.com>
2020-01-28 20:50:08 -0600

    index2.html is the old code for html. Added index.html that will show javascript only DOM manipulation.

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,7 +1,2 @@
-function showTheTime() {
-    var s = moment().tz("America/Los_Angeles").format('hh:mm:ss a');    
-    document.getElementById("time").innerHTML = s;
-}
-
-showTheTime(); // for the first load
-setInterval(showTheTime, 250); // update it periodically
+var currentDateEl = document.querySelector(".time")
+currentDateEl.textContent = (moment().format('LLLL'));
---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+// variables
+var times = ["8","9","10","11","12","1","2","3","4","5","6"]
+var container = document.querySelector(".timecontainer")
+var now = new Date("")
+var hours = now.getHours()
+var textArea = document.querySelector(".textarea")
+
+for (var i = 0; i < times.length; i++) {
+    var timeBlock = document.createElement("div")
+    timeBlock.setAttribute("data-time", times[i]);
+    timeBlock.setAttribute("id", "hour -" + times[i]);
+    timeBlock.textContent = times[i];
+    container.append(timeBlock);
+}
+
+function myFunction() {
+    var textArea = document.querySelector(".textarea").action;
+    document.querySelector(".times").innerHTML = textArea;
+}
+
+//parent element to input element
+
+//data set for past, present, future depends on the time
